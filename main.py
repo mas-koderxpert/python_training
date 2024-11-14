30ea8602c6f6c33f0225a6178171233417a0478c
mas-koderxpert <malavs@koderxpert.com>
2024-11-14 11:56:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Hello world")
+print('''Hello My name is malav soni ...i have completed my information technology degree from silver oak university''')
+print("""It been a long time i have never prgrammed in python before""")
